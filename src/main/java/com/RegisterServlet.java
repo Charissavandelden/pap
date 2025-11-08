84bf7b6a2ea8a13180c771011fdc48089316c380
buffalo-glennbyte <glenn.bakker@topicus.nl>
2025-11-08 20:24:26 +0100

    task-page-flow: Refactor of registration page, see details
    Added hardcoded accounts, newly registered accounts are retained as long as the application is running. Added success and error messages.

--- src/main/java/com/RegisterServlet.java
+++ src/main/java/com/RegisterServlet.java
@@ -2,6 +2,10 @@ package com;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,23 +18,41 @@ public class RegisterServlet extends HttpServlet implements AbstractServletInter
 {
 
 	private static final long serialVersionUID = 1L;
+	
+    private static final HashMap<String, String> USERS = bootstrapHardcodedUsers();
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		addHtmlAndBodyTags(response);
+		addPokemonStyling(response);
 		PrintWriter out = response.getWriter();
 
-		HttpSession session = request.getSession(true);
+        HttpSession session = request.getSession(false);
+        // User is already logged in
+        if (session != null && session.getAttribute("username") != null)
+            response.sendRedirect("/welcome");
+        
+		out.println("<div class='pokemon-container'>");
+		out.println("<h1>Welcome trainer!</h1>");
+		
+		if (Boolean.parseBoolean(request.getParameter("success")))
+            out.println("<p style='color:green'>Registration successful!</p>");
+        
+		if (request.getParameter("error") != null) {
+			String decodedError = decode(request.getParameter("error").toString());
+			out.println("<p style='color:red'>" + decodedError + "</p>");
+		}
 
-		out.println("<h1>Register</h1>");
-
-		out.println("<h2>Login:</h2>");
+		out.println("<h2>Login or register:</h2>");
 		out.println("<form method='POST' action='/register'>");
-		out.println("  <label>Username: <input type='text' name='userName'></label><br>");
-		out.println("  <label>Password: <input type='password' name='password'></label><br>");
-		out.println("  <button type='submit'>Verzenden</button>");
+		out.println("  <label>Username: <input type='text' name='username' required></label>");
+		out.println("  <label>Password: <input type='password' name='password' required></label><br>");
+		
+		out.println("  <button type='submit' name='action' value='login'>Login</button>");
+		out.println("  <button type='submit' name='action' value='register'>Register</button>");
 
 		out.println("</form>");
+		out.println("</div>");
 		closeHtmlAndBodyTags(response);
 	}
 
@@ -38,13 +60,48 @@ public class RegisterServlet extends HttpServlet implements AbstractServletInter
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws IOException
 	{
-		HttpSession session = request.getSession();
-
-		String userName = request.getParameter("userName");
+		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		session.setAttribute("userName", userName);
-		session.setAttribute("password", password);
+		String action = request.getParameter("action");
+		
+		if ("register".equals(action))
+		{
+			if(USERS.containsKey(username))
+				response.sendRedirect("/register?error=" + encode("Username is taken"));
+			else
+			{
+				USERS.put(username, password);
+				response.sendRedirect("/register?success=true");
+			}
+		}
+		else
+		{
+			if(!USERS.containsKey(username) || !USERS.get(username).equals(password))
+				response.sendRedirect("/register?error=" + encode("Invalid credentials!"));
+			
+			HttpSession session = request.getSession(true);
+			session.setAttribute("username", username);
 
-		response.sendRedirect("/welcome");
+			response.sendRedirect("/welcome");
+		}
+	}
+	
+	private String encode(String message)
+	{
+		return URLEncoder.encode(message, StandardCharsets.UTF_8);
+	}
+	
+	private String decode(String message)
+	{
+		return URLDecoder.decode(message, StandardCharsets.UTF_8);
+	}
+	
+	private static HashMap<String, String> bootstrapHardcodedUsers()
+	{
+		HashMap<String, String> standardUsers = new HashMap<String, String>();
+		standardUsers.put("gary", "lilbisch");
+		standardUsers.put("ash_ketchum", "verybest");
+		return standardUsers;
 	}
 }
+
