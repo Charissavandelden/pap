25b37671e2d12c1dd9d994722fc8deec48caa758
buffalo-glennbyte <glenn.bakker@topicus.nl>
2025-11-08 21:17:42 +0100

    task-page-flow: Welcome page now displays latest registered pokemon.

--- src/main/java/entities/Pokemon.java
+++ src/main/java/entities/Pokemon.java
@@ -2,4 +2,37 @@ package entities;
 
 public class Pokemon {
 
+	private String name;
+	
+	private String type;
+	
+	private String move;
+	
+	private int pokedexNumber;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getMove() {
+		return move;
+	}
+	public void setMove(String move) {
+		this.move = move;
+	}
+	public int getPokedexNumber() {
+		return pokedexNumber;
+	}
+	public void setPokedexNumber(int pokedexNumber) {
+		this.pokedexNumber = pokedexNumber;
+	}
+	
 }
